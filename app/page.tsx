67cb92add049d182883e4435678c062bac586fa5
sohel <sohelarfi0@gmail.com>
2026-01-03 23:02:10 +0530

    day-1

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import Image from 'next/image';
 
 const page = () => {
   return (
-    <div>page</div>
+    <main className='main-container'>
+      <section className='home-grid'>
+        <div id="coin-overview">
+          <div className='header pt-2'>
+            <Image src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png" alt="Bitcoin" width={56} height={56}/>
+          <div className='info'>
+            <p>Bitcoin /BTC</p>
+            <h1>$89,113.00</h1>
+          </div>
+          </div>
+        </div>
+      </section>
+    </main>
   )
 }
 
