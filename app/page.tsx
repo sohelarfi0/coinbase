c5e32befbf288e096052dde062f47d7c6cb1e4e6
sohel <sohelarfi0@gmail.com>
2026-01-04 23:41:49 +0530

    day-2

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,58 @@
 import React from 'react'
 import Image from 'next/image';
+import { cn } from '@/lib/utils';
+import DataTable from '@/components/DataTable';
+import { headers } from 'next/headers';
+import { Link, TrendingDown, TrendingUp } from 'lucide-react';
+import { fetcher } from '@/lib/coingecko.actions';
 
-const page = () => {
+
+
+const  columns:DataTableColumn<TrendingCoin>[]=[{
+  header:'Name',
+  cellClassName:'name-cell',
+  cell:(coin)=>{
+    const item=coin.item;
+
+    return(
+      <Link href={`/coins/${item.id}`}>
+        <Image src={item.large} alt={item.name} width={36} height={36}/>
+        <p>{item.name}</p>
+      </Link>
+    )
+  },},
+  {
+    header:'24h Change',
+    cellClassName:'name-cell',
+    cell:(coin)=>{
+      const item=coin.item;
+      const isTrendingUp=item.data.price_change_percentage_24h.usd>0;
+
+      return(
+        <div className={cn('price-change',
+        isTrendingUp?'text-green-500':'text-red-500')}>
+          <p>
+            {isTrendingUp? (
+              <TrendingUp width={16} height={16}/>
+
+            ):<TrendingDown width={16} height={16}/>}
+          </p>
+
+        </div>
+      )
+    }
+
+  }
+,{
+  header:'Price', cellClassName:'price-cell',cell:(coin)=>
+    coin.item.data.price
+},
+
+]
+const page = async() => {
+  const coin=await fetcher<CoinDetailsData>('/coins/bitcoin',{
+     dex-pair-format:'symbol'
+  });
   return (
     <main className='main-container'>
       <section className='home-grid'>
@@ -14,6 +65,8 @@ const page = () => {
           </div>
           </div>
         </div>
+        <p>Trending Coins</p>
+        <DataTable columns={[{header:'Title'},{header:'Price'}]} />
       </section>
     </main>
   )
