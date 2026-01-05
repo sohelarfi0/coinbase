4a91c930b6cc63f914a2cdf4cc4daf1ddc446c84
sohel <sohelarfi0@gmail.com>
2026-01-05 23:43:38 +0530

    day-3

--- components/CandleStickChart.tsx
+++ components/CandleStickChart.tsx
@@ -1,8 +1,10 @@
 // import React, { Children } from 'react'
 'use client';
 
+
 import { PERIOD_BUTTONS } from "@/constants";
-import { useState } from "react";
+import { IChartApi,ISeriesApi } from "lightweight-charts";
+import { useRef,useState } from "react";
 
 const CandlestickChart = (({
   children,
@@ -11,11 +13,32 @@ const CandlestickChart = (({
   height=360,
   initialPeriod='daily',
 }: CandlestickChartProps) => {
-
+  const [period,setPeriod]=useState(initialPeriod);
   const [loading,setLoading]=useState(false);
+  const chartContainerRef=useRef<HTMLDivElement|null>(null);
+  const chartRef=useRef<IChartApi |null>(null);
+  const candelSeriesRef=useRef<ISeriesApi<"Candlestick">|null>(null);
+  const [ohlcData,setOhlcData]=useState<OHLCData[]>(data ?? []);
+
+  const fetchOHLCData=async(selectedPeriod:period)=>{
+    try{
+      await fetcher<OHLCData[]>('/coins/bitcoin/ohlc',{
+        vs_currency:'usd',
+        days:1,
+        interval:'hourly',
+        precision:'full',
+      })catch(e){
+
+      }
+    }
+  };
+
+
 
   const handlePeriodChange=(newPeriod:Period)=>{
     if(newPeriod===period) return;
+
+    setPeriod(newPeriod);
   }
 
   return (
@@ -27,7 +50,9 @@ const CandlestickChart = (({
           text-purple-100/50'>
           Period:</span>
           {PERIOD_BUTTONS.map(({value,label})=>(
-            <button key={value} className="config-button" onClick={()=>{}} disabled={loading}>
+            <button key={value} 
+            className="config-button" onClick={()=>{}}
+             disabled={loading}>
               {label}
             </button>
           ))}
